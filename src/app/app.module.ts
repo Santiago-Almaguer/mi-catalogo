a2a3e2e4913ce967df18dd7d3c0b2b1b97349f18
Santiago Almaguer <santiagoalmaguer.96@gmail.com>
2021-05-03 21:20:51 -0500

    E1 A2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { VistasComponent } from './vistas/vistas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    VistasComponent
   ],
   imports: [
     BrowserModule,
